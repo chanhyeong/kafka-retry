74d00e0df11cfec2782f94ee437696ee2b681217
chanhyeong-cho <chu7825@gmail.com>
2024-04-10 18:04:51 +0900

    Initial commit

---
+++ src/test/java/com/example/kafkaretry/KafkaRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
